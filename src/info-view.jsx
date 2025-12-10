0a7ab895647daa4c1a9e7be7c5f0f77457116e8b
José Expósito <jose.exposito89@gmail.com>
2025-12-10 11:46:08 +0100

    info-view: Add basic info view layout

--- src/info-view.jsx
+++ src/info-view.jsx
@@ -1,15 +1,40 @@
 import React from "react";
-import { Button } from "@patternfly/react-core";
+import { Button, Stack, StackItem, Title } from "@patternfly/react-core";
+import { css } from "@patternfly/react-styles";
+import alignment from "@patternfly/react-styles/css/utilities/Alignment/alignment";
+import flex from "@patternfly/react-styles/css/utilities/Flex/flex";
 import PropTypes from "prop-types";
+import Tux from "./tux.jsx";
 
 const InfoView = ({ architecture, version, trace }) => {
   return (
-    <>
+    <Stack hasGutter className={css(flex.alignItemsCenter)}>
+      <StackItem>
+        <Tux />
+      </StackItem>
+
+      <StackItem className={css(alignment.textAlignCenter)}>
+        <Title headingLevel="h1">
+          Your system encountered a critical issue
+        </Title>
+      </StackItem>
+
+      <StackItem className={css(alignment.textAlignCenter)}>
+        <p>
+          We are sorry, it looks like your system has encountered a critical
+          issue that prevented it from continuing normal operation.
+        </p>
+        <p>Please consider reporting it to get support.</p>
+      </StackItem>
+
+      <StackItem>
+        <Button variant="primary">Report Issue</Button>
+      </StackItem>
+
       <p>{architecture}</p>
       <p>{version}</p>
       <pre>{trace}</pre>
-      <Button variant="primary">Report Error</Button>
-    </>
+    </Stack>
   );
 };
 
