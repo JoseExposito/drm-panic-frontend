80dae9446a012c3ce3fe51c970068a7363e647cb
José Expósito <jose.exposito89@gmail.com>
2025-12-23 13:00:53 +0100

    tests: Add unit tests for the getFedoraVersion() helper

--- tests/bugzilla.test.js
+++ tests/bugzilla.test.js
@@ -1,4 +1,4 @@
-import { isNumeric } from "../src/bugzilla.js";
+import { getFedoraVersion, isNumeric } from "../src/bugzilla.js";
 
 describe("isNumeric()", () => {
   test("Single digit numbers are numerics", () => {
@@ -14,5 +14,27 @@ describe("isNumeric()", () => {
 
   test("Negative numbers are not considered numerics", () => {
     expect(isNumeric("-1")).toBeFalsy();
+  });
+});
+
+describe("getFedoraVersion()", () => {
+  test("Parses valid kernel version", () => {
+    expect(getFedoraVersion("6.17.8-200.fc42.x86_64")).toBe("42");
+  });
+
+  test("Parses single digit kernel version", () => {
+    expect(getFedoraVersion("6.17.8-200.fc2.x86_64")).toBe("2");
+  });
+
+  test("Custom compiled kernels default to Rawhide", () => {
+    expect(getFedoraVersion("6.17.0-rc2")).toBe("rawhide");
+  });
+
+  test("Truncated kernel versions default to Rawhide", () => {
+    expect(getFedoraVersion("6.17.8-200.fc42")).toBe("rawhide");
+  });
+
+  test("Non numeric kernel versions default to Rawhide", () => {
+    expect(getFedoraVersion("6.17.8-200.fcAB.x86_64")).toBe("rawhide");
   });
 });
